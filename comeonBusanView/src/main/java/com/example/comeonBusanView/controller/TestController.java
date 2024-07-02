12d609f2d28bc55e54b23bfeee776810edf6c0c4
권현 <rnjsgus21@naver.com>
2024-07-02 17:37:58 +0900

    api 테스트

--- comeonBusanView/src/main/java/com/example/comeonBusanView/controller/TestController.java
+++ comeonBusanView/src/main/java/com/example/comeonBusanView/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.comeonBusanView.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +12,11 @@ public class TestController {
 		
 		return "test";
 				
+	}
+	
+	@GetMapping("/")
+	public String root2() {
+		return "index";
+		
 	}
 }
