e64bd04e5cc4fc6bad82352ef781779d00542549
kim <kyungmin3946@naver.com>
2024-07-03 11:32:22 +0900

    관광날씨 API 완료

---
+++ comeonBusan/src/main/java/com/example/comeonBusan/controller/CcmController.java
@@ -0,0 +1,14 @@
+package com.example.comeonBusan.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api")
+@CrossOrigin("*")
+public class CcmController {
+	
+	
+	
+
+}
