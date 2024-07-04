ef8cee3da6f60aa0c36000f73931e57a50a9881d
kiminbeom <kods9852@naver.com>
2024-07-04 16:42:42 +0900

    관광지 페이지 작성 리포지토리 이름수정

--- comeonBusan/src/main/java/com/example/comeonBusan/repository/TourlistRepository.java
+++ comeonBusan/src/main/java/com/example/comeonBusan/repository/TourlistRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.comeonBusan.entity.TourList;
 
-public interface ExRepository extends JpaRepository<TourList, String>{
+public interface TourlistRepository extends JpaRepository<TourList, String>{
 
 }
