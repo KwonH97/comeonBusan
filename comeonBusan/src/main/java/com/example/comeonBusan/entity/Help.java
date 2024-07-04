0e75035fd347209a0da4b3fc07fd46f2fd17d3ab
HJK0000 <queen00220@naver.com>
2024-07-04 12:08:57 +0900

    공지사항 엔티티 속성 추가

--- comeonBusan/src/main/java/com/example/comeonBusan/entity/Help.java
+++ comeonBusan/src/main/java/com/example/comeonBusan/entity/Help.java
@@ -1,24 +1,33 @@
 package com.example.comeonBusan.entity;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Help {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long hnum;
-	
 	private String title;
 	private String content;
+	private String himg;
+	private LocalDate regDate;
 	
 	public void update(Help help) {
 		this.title = help.getTitle();
 		this.content = help.getContent();
+		this.himg = help.getHimg();
+		this.regDate = help.getRegDate();
 	}
 }
