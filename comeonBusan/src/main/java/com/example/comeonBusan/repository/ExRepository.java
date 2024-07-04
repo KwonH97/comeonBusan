ae04a0baf33d9b884cfa4cf8b27bd01e0fca601e
kiminbeom <kods9852@naver.com>
2024-07-04 09:57:03 +0900

    repository 수정

--- comeonBusan/src/main/java/com/example/comeonBusan/repository/ExRepository.java
+++ comeonBusan/src/main/java/com/example/comeonBusan/repository/ExRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.comeonBusan.entity.ExEntity;
 
-public interface ExRepository extends JpaRepository<ExEntity, Long>{
+public interface ExRepository extends JpaRepository<ExEntity, String>{
 
 }
