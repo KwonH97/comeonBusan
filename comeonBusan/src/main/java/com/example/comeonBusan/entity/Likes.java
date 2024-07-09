ca8fb047f9c060322c8a25dfe9f1646f825d39dd
권현 <rnjsgus21@naver.com>
2024-07-09 17:54:32 +0900

    Views와 Likes에서 순환참조 숙박 리스트 - 페이지네이션

--- comeonBusan/src/main/java/com/example/comeonBusan/entity/Likes.java
+++ comeonBusan/src/main/java/com/example/comeonBusan/entity/Likes.java
@@ -1,6 +1,13 @@
 package com.example.comeonBusan.entity;
 
-import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
@@ -13,18 +20,22 @@ public class Likes {
 
     @ManyToOne
     @JoinColumn(name = "tourlist_uc_seq", referencedColumnName = "uc_seq")
+    @JsonBackReference
     private TourList tourlist;
 
     @ManyToOne
     @JoinColumn(name = "festival_uc_seq", referencedColumnName = "UC_SEQ")
+    @JsonBackReference
     private Festival festival;
 
     @ManyToOne
     @JoinColumn(name = "lodgment_lid", referencedColumnName = "lid")
+    @JsonBackReference
     private Lodgment lodgment;
     
     @ManyToOne
     @JoinColumn(name = "food_uc_seq", referencedColumnName = "UC_SEQ")
+    @JsonBackReference
     private Food food;
 
     private Long likecount;
