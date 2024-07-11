d24130b0f05b863528c68c2e6e9b547904d3109e
bubby3854 <c1004ds@naver.com>
2024-07-11 09:09:40 +0900

    컨트롤러 수정

--- comeonBusanView/src/main/java/com/example/comeonBusanView/controller/CcmViewController.java
+++ comeonBusanView/src/main/java/com/example/comeonBusanView/controller/CcmViewController.java
@@ -5,46 +5,46 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CcmViewController {
-	
-	@GetMapping("/weather")
-	public String loadWeather() {
-		return "weather";
-	}
-	
-	@GetMapping("/showMap")
-	public String showMap() {
-		return "mapTest";
-	}
-	
-	@GetMapping("/showTheWay")
-	public String showTheWay() {
-		return "showTheWay";
-	}
-	
-	@GetMapping("/roadView")
-	public String roadView() {
-		return "roadView";
-	}
-	
-	@GetMapping("/rainInfo")
-	public String rainInfo() {
-		return "rainInfo";
-	}
-	
-	@GetMapping("/recommand")
-	public String recommand() {
-		return "recommand";
-	}
-	
-	@GetMapping("/translate")
-	public String translate() {
-		return "translate";
-	}
-	
-	@GetMapping("/dangerArea")
-	public String dangerArea() {
-		return "dangerArea";
-	}
+    
+    @GetMapping("/weather")
+    public String loadWeather() {
+        return "weather";
+    }
+    
+    @GetMapping("/showMap")
+    public String showMap() {
+        return "mapTest";
+    }
+    
+    @GetMapping("/showTheWay")
+    public String showTheWay() {
+        return "showTheWay";
+    }
+    
+    @GetMapping("/roadView")
+    public String roadView() {
+        return "roadView";
+    }
+    
+    @GetMapping("/rainInfo")
+    public String rainInfo() {
+        return "rainInfo";
+    }
+    
+    @GetMapping("/recommand")
+    public String recommand() {
+        return "recommand";
+    }
+    
+    @GetMapping("/translate")
+    public String translate() {
+        return "translate";
+    }
+    
+    @GetMapping("/dangerArea")
+    public String dangerArea() {
+        return "dangerArea";
+    }
 
 
 }
