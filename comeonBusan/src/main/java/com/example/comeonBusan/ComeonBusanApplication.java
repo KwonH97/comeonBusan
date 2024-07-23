747141cd1194e6339c436f0e42aa72eea6984f8e
권현 <rnjsgus21@naver.com>
2024-07-23 11:44:07 +0900

    조회수 DB저장 구현, 페스티벌 페이지네이션 통일, @EnableScheduling - > redis 실행시키기

--- comeonBusan/src/main/java/com/example/comeonBusan/ComeonBusanApplication.java
+++ comeonBusan/src/main/java/com/example/comeonBusan/ComeonBusanApplication.java
@@ -2,8 +2,10 @@ package com.example.comeonBusan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ComeonBusanApplication {
 
 	public static void main(String[] args) {
