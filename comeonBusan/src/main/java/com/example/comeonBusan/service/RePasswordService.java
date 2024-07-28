eb309e90388d00e984db7ddf97a9b55e657ed8bd
hjk <queen00220@naver.com>
2024-07-28 10:19:21 +0900

    redisService 및 redisTemplate 필요한 파일 주석처리

--- comeonBusan/src/main/java/com/example/comeonBusan/service/RePasswordService.java
+++ comeonBusan/src/main/java/com/example/comeonBusan/service/RePasswordService.java
@@ -1,47 +1,47 @@
-package com.example.comeonBusan.service;
-
-import java.util.Optional;
-
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.example.comeonBusan.entity.UserEntity;
-import com.example.comeonBusan.repository.UserRepository;
-
-import lombok.RequiredArgsConstructor;
-
-@Service
-@Transactional(readOnly = true)
-@RequiredArgsConstructor
-public class RePasswordService {
-
-	private final UserRepository userRepository;
-	private final PasswordEncoder passwordEncoder;
-	private final RedisService redisService;
-
-	@Transactional
-	public void resetPassword(String uuid, String newPassword) throws Exception {
-
-		// redis에 uuid가 있는지 확인, 없으면 error
-
-		String email = redisService.getValues(uuid);
-		if (email == null) {
-			throw new Exception();
-
-		}
-
-		// redis에서 uuid로 이메일을 찾아온다.
-		Optional<UserEntity> user = userRepository.findByEmail(email);
-		
-		UserEntity result =  user.get();
-		
-		// 비밀번호 재설정
-		result.updatePassword(passwordEncoder.encode((newPassword)));
-
-		// 비밀번호 업데이트 후 redis에서 uuid를 지운다.
-		redisService.deleteValues(uuid);
-
-	}
-
-}
+//package com.example.comeonBusan.service;
+//
+//import java.util.Optional;
+//
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import com.example.comeonBusan.entity.UserEntity;
+//import com.example.comeonBusan.repository.UserRepository;
+//
+//import lombok.RequiredArgsConstructor;
+//
+//@Service
+//@Transactional(readOnly = true)
+//@RequiredArgsConstructor
+//public class RePasswordService {
+//
+//	private final UserRepository userRepository;
+//	private final PasswordEncoder passwordEncoder;
+//	private final RedisService redisService;
+//
+//	@Transactional
+//	public void resetPassword(String uuid, String newPassword) throws Exception {
+//
+//		// redis에 uuid가 있는지 확인, 없으면 error
+//
+//		String email = redisService.getValues(uuid);
+//		if (email == null) {
+//			throw new Exception();
+//
+//		}
+//
+//		// redis에서 uuid로 이메일을 찾아온다.
+//		Optional<UserEntity> user = userRepository.findByEmail(email);
+//		
+//		UserEntity result =  user.get();
+//		
+//		// 비밀번호 재설정
+//		result.updatePassword(passwordEncoder.encode((newPassword)));
+//
+//		// 비밀번호 업데이트 후 redis에서 uuid를 지운다.
+//		redisService.deleteValues(uuid);
+//
+//	}
+//
+//}
